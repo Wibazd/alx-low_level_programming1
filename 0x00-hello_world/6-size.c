2ab7bf20b3e8e9e02d6ad477b9ceae368c5dad7e
Wibazd <wahibazd14@gmail.com>
2023-02-16 11:58:05 -0800

    hhhhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - come on
+ * main - Guess What !!??  This is a Description
  * Return: 0 (Success)
  */
 
@@ -17,5 +17,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+return (0);
 }
