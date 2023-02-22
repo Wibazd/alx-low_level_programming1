1b78b84a478afddf9d7476911c305aef71935339
Wibazd <wahibazd14@gmail.com>
2023-02-21 18:26:45 -0800

    Hello alx

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: integer value
+ *
+ * Return: void
  */
-
 void print_times_table(int n)
 {
+	int i, j, res;
 
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-        return;
-
-while (a <= n)
-{
-        for (b = 0; b <= n; b++)
-        {
-                rep = a * b;
-                if (b == 0)
-                        _putchar('0' + rep);
-                else if (rep < 10)
-                {
-                        _putchar(' ');
-                        _putchar(' ');
-                        _putchar('0' + rep);
-                }
-                else if (rep < 100)
-                {
-                        _putchar(' ');
-                        _putchar('0' + rep / 10);
-                        _putchar('0' + rep % 10);
-                }
-                else
-                {
-                        _putchar('0' + rep / 100);
-                        _putchar('0' + (rep - 100) / 10);
-                        _putchar('0' + rep % 10);
-                }
-                if (b < n)
-                {
-                        _putchar(',');
-                        _putchar(' ');
-                }
-        }
-        _putchar('\n');
-        a++;
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				res = i * j;
+				if (res < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else if (res < 100)
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else
+				{
+					_putchar(res / 100 + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
