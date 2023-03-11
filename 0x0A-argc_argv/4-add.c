e3f876d5a2824336493887411c388ad1cc3eadc2
Wibazd <wahibazd14@gmail.com>
2023-03-10 18:16:45 -0800

    Of

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,11 +6,9 @@
  * @argv: Arguments Vector
  * Return: 1 or 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i, sum = 0, count = 1, d = 1;
-
 	while (argv[d])
 	{
 		if (*argv[d] < 48 || *argv[d] > 57)
@@ -19,13 +17,11 @@ int main(int argc, char *argv[])
 		}
 		d++;
 	}
-
 	if (argc == count)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
 		if (*argv[i] >= 48 && *argv[i] <= 57)
@@ -35,7 +31,6 @@ int main(int argc, char *argv[])
 		else
 		{
 			printf("Error\n");
-
 			return (1);
 		}
 	}
