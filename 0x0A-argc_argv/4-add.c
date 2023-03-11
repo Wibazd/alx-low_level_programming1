8714337f9894bdebbc35eeee686f80de27e0e082
Wibazd <wahibazd14@gmail.com>
2023-03-10 18:20:38 -0800

    Offfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,9 +6,11 @@
  * @argv: Arguments Vector
  * Return: 1 or 0
  */
+
 int main(int argc, char *argv[])
 {
 	int i, sum = 0, count = 1, d = 1;
+
 	while (argv[d])
 	{
 		if (*argv[d] < 48 || *argv[d] > 57)
@@ -17,11 +19,13 @@ int main(int argc, char *argv[])
 		}
 		d++;
 	}
+
 	if (argc == count)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
 		if (*argv[i] >= 48 && *argv[i] <= 57)
@@ -31,6 +35,7 @@ int main(int argc, char *argv[])
 		else
 		{
 			printf("Error\n");
+
 			return (1);
 		}
 	}
