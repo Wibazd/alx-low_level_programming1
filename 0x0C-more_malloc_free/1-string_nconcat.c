c3016e7483ae31daae9f16aa238560c75ddb29b9
Wibazd <wahibazd14@gmail.com>
2023-03-20 16:08:15 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - function allocate memorey = sizeof(s1 + n) bytes to
- * concatenate s1 and s2 in it
- *
- * @s1: first string
- * @s2: second string
- * @n: number of bytes from s2 to be concatenated with s1
- *
- * Return: pointer to the new conatenated string
- */
-
+* string_nconcat - prints concatenate string;
+* @s1: input string.
+* @s2: input string.
+* @n: len s2 string for print.
+* Return: Nothing.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, cnt = 0;
-	char *ptr;
+	unsigned int l1, i, e;
+	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+	l1 = 0;
+	while (s1[l1])
+		l1++;
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
+	a = malloc(sizeof(*a) * l1 + n + 1);
 
-	if (j > n)
-		j = n;
-
-	ptr = malloc(((i + j) + 1) * sizeof(char));
-	if (ptr == NULL)
+	if (a == NULL)
 		return (NULL);
 
-	for (cnt = 0; cnt < i; cnt++)
-		ptr[cnt] = s1[cnt];
-	for (i = 0; i < j; i++)
+	for (i = 0, e = 0; i < (l1 + n); i++)
 	{
-		ptr[cnt] = s2[i];
-		cnt ++;
+		if (i < l1)
+		{
+			a[i] = s1[i];
+		}
+		else
+		{
+			a[i] = s2[e++];
+		}
 	}
-	ptr[cnt] = '\0'; 
-	return (ptr);
+	a[i] = '\0';
+	return (a);
 }
