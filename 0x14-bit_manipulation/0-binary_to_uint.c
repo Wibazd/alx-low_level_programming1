ddc0023b2eadd53016b0acc50679ba9cac4abdae
Wibazd <wahibazd14@gmail.com>
2023-04-10 10:51:55 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,25 +4,25 @@
 /**
 * binary_to_uint - a function that converts a
 *binary number to an unsigned int
-* @W: pointing to a string of 0 and 1 chars
+* @b: pointing to a string of 0 and 1 chars
 * Return: the converted number or 0
 **/
 
-unsigned int binary_to_uint(const char *W)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, int_n;
 
 	int_n = 0;
 	i = 0;
-	if (!W)
+	if (!b)
 		return (0);
 
-	while (W[i] != '\0')
+	while (b[i] != '\0')
 	{
-		if (W[i] != '0' && W[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		int_n <<= 1;
-		if (W[i] & 1)
+		if (b[i] & 1)
 			int_n += 1;
 		i += 1;
 	}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-number int binary_to_uint(const char *W);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
