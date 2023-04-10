a9cf225b83199b527de25f3ea3aad7361df30389
Wibazd <wahibazd14@gmail.com>
2023-04-10 10:26:15 -0700

    done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* binary_to_uint - a function that converts a
+*binary number to a number int
+* @W: pointing to a string of 0 and 1 chars
+* Return: the converted number or 0
+**/
+
+number int binary_to_uint(const char *W)
+{
+	number int i, int_n;
+
+	int_n = 0;
+	i = 0;
+	if (!W)
+		return (0);
+
+	while (W[i] != '\0')
+	{
+		if (W[i] != '0' && W[i] != '1')
+			return (0);
+		int_n <<= 1;
+		if (W[i] & 1)
+			int_n += 1;
+		i += 1;
+	}
+	return (int_n);
+}
